e19f9a4535038756c162c4594ef3790ed047d648
guidoleen <guidoleen@MacBook-Air.local>
2018-06-04 16:10:05 +0200

    Test

--- src/nl/hva/miw/robot/cohort12/Marvin.java
+++ src/nl/hva/miw/robot/cohort12/Marvin.java
@@ -24,7 +24,7 @@ public class Marvin {
 	private void run() {
 		TextLCD display = brick.getTextLCD();
 		display.drawString("Welkom!", 0, 3);
-		display.drawString("Team Zero", 0, 4);
+		display.drawString("Team A", 0, 4);
 		waitForKey(Button.ENTER);
 	}
 	
