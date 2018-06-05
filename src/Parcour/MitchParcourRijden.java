30a90735ce4359013dda7fef4d47d6857decc746
mitchellsitohang <mitchellsitohang@10.19.0.207>
2018-06-05 18:34:06 +0200

    Class copy aangemaakt

--- src/Parcour/MitchParcourRijden.java
+++ src/Parcour/MitchParcourRijden.java
@@ -22,7 +22,6 @@ public class MitchParcourRijden {
 		float ambient;
 		float white;
 		float black;
-		float test;
 		
 		// create two motor objects to control the motors.
         UnregulatedMotor motorL = new UnregulatedMotor(MotorPort.B);
