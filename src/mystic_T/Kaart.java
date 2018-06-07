9192c8f6a3f2ffaeb5c28d4a7aa87bdf5fbac0da
Laila <lailakee@hotmail.com>
2018-06-07 11:10:50 +0200

    Testen van klaurkaarten naar array

--- src/mystic_T/Kaart.java
+++ src/mystic_T/Kaart.java
@@ -2,16 +2,22 @@ package mystic_T;
 
 public class Kaart {
 	//variabelen die we nodig hebben
+	String naamKaart;
 	int rood;
 	int blauw;
 	int groen;
 	
 	//constructor
-	public Kaart(int rood, int blauw, int groen) {
+	public Kaart(String naamKaart, int rood, int blauw, int groen) {
 		super();
+		this.naamKaart = naamKaart;
 		this.rood = rood;
 		this.blauw = blauw;
 		this.groen = groen;
+	}
+
+	public String getNaamKaart() {
+		return naamKaart;
 	}
 
 	public int getRood() {
--- src/mystic_T/Mystic_T_Launcher.java
+++ src/mystic_T/Mystic_T_Launcher.java
@@ -14,13 +14,16 @@ import java.util.List;
 public class Mystic_T_Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		ColorSensor color = new ColorSensor(SensorPort.S3);
 		int rood;
 		int blauw;
 		int groen;
 		ArrayList<Kaart> kaarten = new ArrayList<>();
+		ArrayList<Kaart> tarotkaarten = new ArrayList<Kaart>();
+		tarotkaarten.add (new Kaart("Rood", 57,5,4));
+		tarotkaarten.add (new Kaart("Groen", 11,35,8));
+		tarotkaarten.add (new Kaart("Blauw", 8,38,38));
 		
         System.out.println("Mystic T");
         Lcd.print(2, "Druk een knop");
@@ -37,33 +40,71 @@ public class Mystic_T_Launcher {
         color.setFloodLight(Color.WHITE);
         
         Color rgb;
+       
+        Lcd.clear(3);
+        Lcd.print(3, "Scan een kaart");
+        Button.waitForAnyPress();
         
         do
        {
             rgb = color.getColor();
-            Lcd.clear(3);
-            Lcd.print(3, "r=%d g=%d b=%d", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
-            Lcd.print(4, "Scan een kaart");
-            Button.waitForAnyPress();
+            Lcd.clear(4);
+            Lcd.clear(5);
+            Lcd.clear(6);
+            Lcd.print(4, "r=%d g=%d b=%d", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
             rood = rgb.getRed();
             groen = rgb.getGreen();
             blauw = rgb.getBlue();
-            kaarten.add (new Kaart(rood, blauw, groen));
-            Delay.msDelay(250);
+            
+            //checken of de kaart in de tarotarray voorkomt
+            for (Kaart kaart : tarotkaarten ) {
+            	if (rood == kaart.getRood()) {
+            		Lcd.print(5, "Dit is een rode kaart");
+            		Sound.beep();
+                    kaarten.add (new Kaart(kaart.getNaamKaart(),rood, blauw, groen));
+                    Lcd.print(6, "Scan volgende kaart");
+                    Button.waitForAnyPress();
+            	} else if(blauw == kaart.getBlauw()) {
+            		Lcd.print(5, "Dit is een blauwe kaart");
+            		Sound.beep();
+                    kaarten.add (new Kaart(kaart.getNaamKaart(), rood, blauw, groen));
+                    Lcd.print(6, "Scan volgende kaart");
+                    Button.waitForAnyPress();
+            	} else if (groen == kaart.getGroen()) {
+            		Lcd.print(5, "Dit is een groene kaart");
+            		Sound.beep();
+                    kaarten.add (new Kaart(kaart.getNaamKaart(), rood, blauw, groen));
+                    Lcd.print(6, "Scan volgende kaart");
+                    Button.waitForAnyPress();
+            	}
+            } 
        } while (kaarten.size()<3);
-            //}while (Button.ESCAPE.isUp());
+        
+        //kaarten scannen is klaar
+        Lcd.print(6, "Alle kaarten zijn gescand");
+        Button.waitForAnyPress();
+        
+        for (int i =0; i<kaarten.size(); i++ ) {
+         Lcd.clear(i+3);
+         Lcd.print(i+3, kaarten.get(i).getNaamKaart());
+        	
+        }
+        
 
         //Weet niet of deze nodig is
         Delay.msDelay(1000);
 		
-        for(int i = 0; i<kaarten.size(); i++) {
-        	if (kaarten.get(i).getRood() == kaarten.get(i+1).getRood()){
-        		Sound.beepSequence();
-        	}
+        //Deze forloop checkt de kleuren van de 3 kaarten met de al bestaande lijst)
+        //for(int i = 0; i<kaarten.size(); i++) {
+        	//if (kaarten.get(i).getRood() == kaarten.get(i+1).getRood()){
+        		//Sound.beepSequence();
+        	//}
         	//kaarten.get(i).getBlauw();
         	//kaarten.get(i).getGroen();
-        }
+       // }
         
+	
+	
         //Einde
         color.close();
         
