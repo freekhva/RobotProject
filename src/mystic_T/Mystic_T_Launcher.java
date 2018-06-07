fb4ea445a3d30350ff7935d3b4bd87e40b8032dc
Laila <lailakee@hotmail.com>
2018-06-07 10:08:22 +0200

    Klassen van Mystic T ingevuld

--- src/mystic_T/Mystic_T_Launcher.java
+++ src/mystic_T/Mystic_T_Launcher.java
@@ -8,6 +8,8 @@ import lejos.hardware.Sound;
 import lejos.hardware.port.SensorPort;
 import lejos.robotics.Color;
 import lejos.utility.Delay;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mystic_T_Launcher {
 
@@ -15,7 +17,11 @@ public class Mystic_T_Launcher {
 		// TODO Auto-generated method stub
 
 		ColorSensor color = new ColorSensor(SensorPort.S3);
-
+		int rood;
+		int blauw;
+		int groen;
+		ArrayList<Kaart> kaarten = new ArrayList<>();
+		
         System.out.println("Mystic T");
         Lcd.print(2, "Druk een knop");
         
@@ -32,17 +38,40 @@ public class Mystic_T_Launcher {
         
         Color rgb;
         
-        while (Button.ESCAPE.isUp())
-        {
+        do
+       {
             rgb = color.getColor();
-            
-            Lcd.clear(6);
-            Lcd.print(6, "r=%d g=%d b=%d", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+            Lcd.clear(3);
+            Lcd.print(3, "r=%d g=%d b=%d", rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+            Lcd.print(4, "Scan een kaart");
+            Button.waitForAnyPress();
+            rood = rgb.getRed();
+            groen = rgb.getGreen();
+            blauw = rgb.getBlue();
+            kaarten.add (new Kaart(rood, blauw, groen));
             Delay.msDelay(250);
-        }
+       } while (kaarten.size()<3);
+            //}while (Button.ESCAPE.isUp());
 
+        //Weet niet of deze nodig is
         Delay.msDelay(1000);
 		
+        for(int i = 0; i<kaarten.size(); i++) {
+        	if (kaarten.get(i).getRood() == kaarten.get(i+1).getRood()){
+        		Sound.beepSequence();
+        	}
+        	//kaarten.get(i).getBlauw();
+        	//kaarten.get(i).getGroen();
+        }
+        
+        //Einde
+        color.close();
+        
+        Sound.beepSequence();    // we are done.
+
+        Button.LEDPattern(4);
+        Button.waitForAnyPress();
+        
 	}
 
 }
